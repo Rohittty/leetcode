527a5b6de823a0dabcb20b8ca04eea60b6dce4a4
Rohit Adwani <94852691+Rohittty@users.noreply.github.com>
2024-10-09 18:36:14 +0530

    Time: 3 ms (42.94%), Space: 7.8 MB (81.46%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-       stack<int>st;
+       int size=0;
         int unbalanced=0;
         for(int i=0;i<s.size();i++){
-          if(s[i]=='(')st.push(s[i]);
-            else if(!st.empty())st.pop();
+          if(s[i]=='(')size++;
+            else if(size>0)size--;
             else
                 unbalanced++;
         }
-        return st.size()+unbalanced;
+        return size+unbalanced;
     }
 };
