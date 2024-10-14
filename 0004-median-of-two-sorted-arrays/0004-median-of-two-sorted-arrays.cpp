550bbf9574789d018689d94176e90224947a877a
Rohit Adwani <94852691+Rohittty@users.noreply.github.com>
2024-10-14 18:31:04 +0530

    Time: 28 ms (41.58%), Space: 95 MB (46.22%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-       for(int i=0;i<nums2.size();i++){
-           nums1.push_back(nums2[i]);
-       }
-        sort(nums1.begin(),nums1.end());
+        int n=nums1.size();
+        int m=nums2.size();
+        int k=n+m;
+       vector<int>op(k,0);
+        int i=0;
+        int j=0;
+        int h=0;
+        while(h<k){
+            if(i>=nums1.size()){
+                op[h]=nums2[j];
+                j++;
+            }
+            else if(j>=nums2.size()){
+                op[h]=nums1[i];
+                i++;
+            }
+            else{
+                if(nums1[i]<nums2[j]){
+                    op[h]=nums1[i];
+                    i++;
+                }
+                else{
+                   op[h]=nums2[j];
+                    j++;
+                }
+                      
+                }
+            h++;
+        }
         double ans=0;
-        if(nums1.size()%2!=0){
-            ans=nums1[nums1.size()/2];
+        if(k%2!=0){
+          ans=op[k/2];
         }
         else{
-            int a=(nums1.size()-1)/2;
+            int a=(k-1)/2;
             int z=a+1;
-            
-            cout<<nums1[a]<<nums1[z];
-            ans=(double)(nums1[a]+nums1[z])/2;
-            cout<<ans;
+            ans=(double)(op[a]+op[z])/2;
         }
         return ans;
     }
