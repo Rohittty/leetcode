d32142caf2e3dd9f87967cd8a0a03ce18a5dbaff
Rohit Adwani <94852691+Rohittty@users.noreply.github.com>
2024-10-17 18:12:39 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (31.77%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        int g=num;
+        string ans="";
+        while(num!=0){
+        int r=num%10;
+            
+            ans.push_back(r+'0');
+            num=num/10;
+            
+        }
+        int k=0;
+        int idx=0;
+        reverse(ans.begin(),ans.end());
+        for(int i=0;i<ans.size();i++){
+             int maxi=0;
+            for(int j=i+1;j<ans.size();j++){
+                maxi=max(ans[j]-'0',maxi);
+                
+            }
+            if(maxi>ans[i]-'0'){
+                k=maxi;
+                idx=i;
+                break;
+            }
+        }
+        vector<int>l;
+        if(k==0)return g;
+        for(int i=0;i<ans.size();i++){
+              if(idx<i&&ans[i]-'0'==k){
+                  l.push_back(i);
+                 
+              }
+        }
+        int op=0;
+        
+        for(int i=0;i<l.size();i++)op=max(op,l[i]);
+        swap(ans[idx],ans[op]);
+        int r=stoi(ans);
+        
+        return r;
+    }
+};
