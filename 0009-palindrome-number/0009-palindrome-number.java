4dfabf8ed1ba92b3ddd34469d59dc5843a2f7c15
Rohit Adwani <94852691+Rohittty@users.noreply.github.com>
2024-10-28 17:56:51 +0530

    Time: 5 ms (74.99%), Space: 43.9 MB (63.71%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
          Long count = new Long(0);
-        int duplicate= x;;
+        int duplicate= x;
         while(x>0){
            int lastdigit= x%10;
             count=(count*10)+lastdigit;
