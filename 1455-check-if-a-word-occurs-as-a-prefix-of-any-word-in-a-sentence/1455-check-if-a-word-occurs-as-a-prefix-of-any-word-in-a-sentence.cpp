8dcdf4e10222a6bbe44d3450a5f8dc4796208dc9
Rohit Adwani <94852691+Rohittty@users.noreply.github.com>
2024-12-02 14:50:43 +0530

    Time: 5 ms (5.43%), Space: 8.2 MB (6.42%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -3,7 +3,6 @@ public:
     int isPrefixOfWord(string sen, string sea) {
      int cs=1;
        int j=0;
-        int b=0;
         for(int i=0;i<sen.size();i++){
              if(sen[i]==' ')cs++;
           if(sen[i]==sea[j])j++;
@@ -11,13 +10,12 @@ public:
                 j=0;
             cout<<i<<j<<endl;
             if(j==sea.length() && (i-sea.length()+1==0||sen[i-j]==' ')){
-                  b=1;
-                break;
+                  return cs;
+            
             }
            
         }
-        if(b==0)return -1;
-        return cs;
+        return -1;
         
     }
 };
